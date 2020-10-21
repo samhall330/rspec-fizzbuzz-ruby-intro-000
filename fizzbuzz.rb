54d2a2216d993877a7b862e13644738cf0ecee79
Sam Hall <mann.samantha11@gmail.com>
2020-10-21 17:41:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,21 +3,13 @@ def fizzbuzz
 end
 
 def fizzbuzz(int)
-<<<<<<< HEAD
+
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
   elsif int % 3 == 0
     return "Fizz"
-=======
-  if int % 3 == 0
-    return "Fizz"
-  elsif int % 5 == 0
-    return "Buzz"
-  elsif int % 5 == 0 && int % 3 == 0
-    return "FizzBuzz"
->>>>>>> 1009bbf4ffba2b76824d54eff10932d961a8acaa
   else
     return nil
   end
